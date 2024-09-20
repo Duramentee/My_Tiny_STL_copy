00393995dde4a059621008c7f0e584b966680471
Kojimamemes <lsx3193161830@163.com>
2024-09-20 20:51:30 +0800

    完成了swap

--- src/util.h
+++ src/util.h
@@ -38,11 +38,36 @@ T&& forward(typename std::remove_reference<T>::type&& arg) noexcept
 	return static_cast<T&&>(arg);
 }
 
+//swap
 
+template <class Tp>
+void swap(Tp& lhs, Tp& rhs)
+{
+	auto tmp(mystl::move(lhs));
+	lhs = mystl::move(rhs);
+	rhs = mystl::move(tmp);
 }
 
+template <class ForwardIter1, class ForwardIter2>
+ForwardIter2 swap_range(ForwardIter1 first1, ForwardIter1 last1, ForwardIter2 first2)
+{
+	for (; first1 != last1; ++first, (void) ++first2)
+	{
+		mystl::swap(*first1, *first2);
+	}
+	return first2;
+}
+
+template <class Tp, size_t N>
+void swap(Tp(&a)[N], Tp(&b)[N])
+{
+	mystl::swap_range(a, a + N, b);
+}
+
+} // namespace mystl
 
 
 
 
-#endif  //! MYTINYSTL_UTIL_H_
+
+#endif // !MYTINYSTL_UTIL_H_
